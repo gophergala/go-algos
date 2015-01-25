fae5fa4a2bd134b84898f946bb670e25154c6efa
Michael Ye <m6ye@uwaterloo.ca>
2015-01-25 18:34:50 -0500

    add binary search

---
+++ search/binary-search.go
@@ -0,0 +1,20 @@
+package search
+
+// source: http://rosettacode.org/wiki/Binary_search#Go
+
+// iterative version
+func binarySearch(a []float64, value float64) int {
+    low := 0
+    high := len(a) - 1
+    for low <= high {
+        mid := (low + high) / 2
+        if a[mid] > value {
+            high = mid - 1
+        } else if a[mid] < value {
+            low = mid + 1
+        } else {
+            return mid
+        }
+    }
+    return -1
+}
