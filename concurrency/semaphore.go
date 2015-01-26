7a75cbe272365f42e78dca8664954bc99beca996
Michael Ye <m6ye@uwaterloo.ca>
2015-01-25 19:17:03 -0500

    add semaphore

---
+++ concurrency/semaphore.go
@@ -0,0 +1,39 @@
+package concurrency
+
+import "runtime"
+
+type Semaphore struct {
+	counter int
+	lock    concurrency.SpinLock
+}
+
+func newSemaphore(n int) *Semaphore {
+	sem := new(Semaphore)
+	sem.counter = n
+
+	return sem
+}
+
+func newSemaphore() *Semaphore {
+	sem := new(Semaphore)
+
+	return sem
+}
+
+func P() {
+	lock.acquire()
+
+	while counter == 0 {
+		runtime.Gosched() // yield to other routines
+	}
+
+	lock.release()
+}
+
+func V() {
+	lock.acquire()
+
+	counter++
+
+	lock.release()
+}
