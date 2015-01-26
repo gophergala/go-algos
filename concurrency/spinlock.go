a36452737e70dd642f2ada84148e8315d46be0c2
Michael Ye <m6ye@uwaterloo.ca>
2015-01-25 19:03:55 -0500

    wrote spinlock

--- concurrency/spinlock.go
+++ concurrency/spinlock.go
@@ -1,5 +1,29 @@
 package concurrency
 
-struct SpinLock {
+import (
+	"runtime"
+	"sync/atomic"
+)
 
+const (
+	UNLOCKED = 0
+	LOCKED   = 1
+)
+
+type SpinLock struct {
+	locked int32
+}
+
+func (sp *SpinLock) acquire() {
+	while !sp.tryAcquire() { // if can't get the lock -- spin
+		runtime.Gosched() // yield to other routines
+	}
+}
+
+func (sp *SpinLock) tryAcquire() bool {
+	return atomic.CompareAndSwapInt32((sp.locked,UNLOCKED, LOCKED))
+}
+
+func (sp *SpinLock) release() {
+	sp.locked = UNLOCKED
 }
