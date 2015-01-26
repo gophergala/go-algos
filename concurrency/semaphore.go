f8fbbfe9491ef785be7967205e2ee62ec8673ec0
Michael Ye <m6ye@uwaterloo.ca>
2015-01-25 19:26:46 -0500

    added cond and waitgroup

--- concurrency/semaphore.go
+++ concurrency/semaphore.go
@@ -10,31 +10,25 @@ type Semaphore struct {
 func newSemaphore(n int) *Semaphore {
 	sem := new(Semaphore)
 	sem.counter = n
-
 	return sem
 }
 
 func newSemaphore() *Semaphore {
 	sem := new(Semaphore)
-
 	return sem
 }
 
 func P() {
 	lock.acquire()
-
 	while counter == 0 {
 		runtime.Gosched() // yield to other routines
 	}
 	counter--
-
 	lock.release()
 }
 
 func V() {
 	lock.acquire()
-
 	counter++
-
 	lock.release()
 }
